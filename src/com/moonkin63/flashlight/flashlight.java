06a26c2061786b903dd3ff4b7bffd154101c65eb
moonkin63 <moonkin63@hotmail.com>
2014-01-19 19:36:16 +0400

    don't turned off display when flashlight turned on

--- src/com/moonkin63/flashlight/flashlight.java
+++ src/com/moonkin63/flashlight/flashlight.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class flashlight extends Activity {
+    /**
+     * when status = false then flashlight is turned off
+     */
     private boolean status = false;
     private Camera cam;
 
@@ -17,8 +21,6 @@ public class flashlight extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-
 
         final Button bt = (Button) findViewById(R.id.buttonTurnOn);
         bt.setOnClickListener(new View.OnClickListener() {
@@ -32,16 +34,25 @@ public class flashlight extends Activity {
                     cam.stopPreview();
                     cam.release();
                 } else {
-                    cam =  Camera.open();
+                    cam = Camera.open();
                     bt.setText(R.string.turn_off);
                     Camera.Parameters p = cam.getParameters();
                     p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
                     cam.setParameters(p);
                     cam.startPreview();
                 }
+                updateScreenState();
                 status = !status;
             }
         });
+    }
+
+    private void updateScreenState() {
+        if (status) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        } else {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
     }
 
     @Override
